cd8875b346813ec91f9c21b5d659d89d1a3f82d5
Renaísa <renaisaalves@gmail.com>
2022-03-17 13:03:12 -0300

    Updates

---
+++ 2.1.py
@@ -0,0 +1,8 @@
+# Escreva expressões algébricas Python correspondentes aos seguintes comandos:
+# (a) A soma dos 5 primeiros inteiros positivos.
+# (b) A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
+# (c) O número de vezes que 73 cabe em 403.
+# (d) O resto de quando 403 é dividido por 73.
+# (e) 2 à 10ª potência.
+# (f) O valor absoluto da distância entre a altura de Sara (54 polegadas) e a altura de Mark (57 polegadas).
+# (g) O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.
--- aula1-univesp.py
+++ aula1-univesp.py
@@ -1,7 +1,3 @@
 print('=' * 50)
 print(f'{"PRIMEIRA AULA DE PYTHON PELA UNIVESP!":^50}')
 print('=' * 50)
-
-num = int(input('Número: '))
-result = num // 2
-print(result)

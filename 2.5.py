bbcc875101602ecc5db38adf293b293d8988228d
Renaísa <renaisaalves@gmail.com>
2022-03-17 13:07:57 -0300

    Create 2.5.py

---
+++ 2.5.py
@@ -0,0 +1,8 @@
+#Comece executando a atribuição:
+#s = '0123456789'
+#Agora, escreva expressões usando a string s e o operador de indexação que é avaliado como:
+#(a) '0'
+#(b) '1'
+#(c) '6'
+#(d) '8'
+#(e) '9'

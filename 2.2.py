cc63c666e7e8ba5fd7d66e8ef7da1bf9f53e1dda
Renaísa <renaisaalves@gmail.com>
2022-03-17 13:04:13 -0300

    Updates

---
+++ 2.2.py
@@ -0,0 +1,8 @@
+#Traduza os comandos a seguir para expressões Booleanas em Python e avalie-as:
+#(a) A soma de 2 e 2 é menor que 4.
+#(b) O valor de 7 // 3 é igual a 1 + 1.
+#(c) A soma de 3 ao quadrado e 4 ao quadrado é igual a 25.
+#(d) A soma de 2, 4 e 6 é maior que 12.
+#(e) 1387 é divisível por 19.
+#(f) 31 é par. (Dica: o que o resto lhe diz quando você divide por 2?)
+#(g) O preço mais baixo dentre R$ 34,99, R$ 29,95 e R$ 31,50 é menor que R$ 30,00.*

fb3c632cd87a7d1442fc3fe4f5789e77af8f3c03
Renaísa <renaisaalves@gmail.com>
2022-03-17 14:26:37 -0300

    Update 2.5.py

--- 2.5.py
+++ 2.5.py
@@ -1,6 +1,6 @@
-#Comece executando a atribuição:
-#s = '0123456789'
-#Agora, escreva expressões usando a string s e o operador de indexação que é avaliado como:
+print('Comece executando a atribuição:')
+s = '0123456789'
+print('Agora, escreva expressões usando a string s e o operador de indexação que é avaliado como:')
 #(a) '0'
 #(b) '1'
 #(c) '6'

06749b324300dbb017fe4c33a9e8a11f6b7b71fd
Renaísa <renaisaalves@gmail.com>
2022-03-17 14:25:57 -0300

    Update 2.4.py

--- 2.4.py
+++ 2.4.py
@@ -1,10 +1,10 @@
-#Comece executando as instruções de atribuição:
-#>>> s1 = 'ant'
-#>>> s2 = 'bat'
-#>>> s3 = 'cod'
-#Escreva expressões Python usando s1, s2 e s3 e os operadores + e * a fim de avaliar para:
-#(a) 'ant bat cod'
-#(b) ant ant ant ant ant ant ant ant ant ant'
-#(c) 'ant bat bat cod cod cod'
-#(d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
-#(e) 'batbatcod batbatcod batbatcod batbatcod batbatcod'
+print('Comece executando as instruções de atribuição:')
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+print('Escreva expressões Python usando s1, s2 e s3 e os operadores + e * a fim de avaliar para:')
+print('')#(a) 'ant bat cod'
+print('')#(b) ant ant ant ant ant ant ant ant ant ant'
+print('')#(c) 'ant bat bat cod cod cod'
+print('')#(d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+print('')#(e) 'batbatcod batbatcod batbatcod batbatcod batbatcod'

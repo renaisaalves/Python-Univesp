261f3f8d9f6c03be354deb5a37199ec55326b031
Renaísa <renaisaalves@gmail.com>
2022-03-17 13:41:58 -0300

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -1,5 +1,13 @@
 # Escreva expressões algébricas Python correspondentes aos seguintes comandos:
+
 # (a) A soma dos 5 primeiros inteiros positivos.
+
+soma = 0 
+for c in range(1, 6):
+    num = int(input(f'Nº {c}: '))
+    soma = soma + num
+print(f'A soma dos 5 primeiros números foi: {soma}')
+
 # (b) A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
 # (c) O número de vezes que 73 cabe em 403.
 # (d) O resto de quando 403 é dividido por 73.

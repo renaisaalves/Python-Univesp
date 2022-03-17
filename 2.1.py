2c002b7881a3a0d708647ff1a953692c1c07c3e1
Renaísa <renaisaalves@gmail.com>
2022-03-17 13:56:18 -0300

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -9,8 +9,26 @@ for c in range(1, 6):
 print(f'A soma dos 5 primeiros números foi: {soma}')
 
 # (b) A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
+
+Sara = 23
+Mark = 19
+Fatima = 31
+media = Sara + Mark + Fatima / 3
+print(f'A média de idade entre Sara, Mark e Fátima é {media}')
+
 # (c) O número de vezes que 73 cabe em 403.
+
+num = int(input('Número: '))
+numax = int(input('Número máximo: '))
+espaço = numax // num
+print(f'O número {num} cabe {espaço} vezes em {numax}')
+
 # (d) O resto de quando 403 é dividido por 73.
+
+dividendo = int(input('Número: '))
+divisor = int(input(''))
+resto = dividendo % divisor 
+print(f'O resto da divisão {dividendo} por {divisor} é {resto}')
 # (e) 2 à 10ª potência.
 # (f) O valor absoluto da distância entre a altura de Sara (54 polegadas) e a altura de Mark (57 polegadas).
 # (g) O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.

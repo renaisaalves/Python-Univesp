bc99f453508b2566317c69be365c8f9abc27c6b9
Renaísa <renaisaalves@gmail.com>
2022-03-19 17:39:38 -0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,5 +1,11 @@
 #Escreva instruções Python que correspondem às ações a seguir e execute-as:
 
 print('(a) Atribua o valor inteiro 3 à variável a.')
+a = int(3)
+
 print('(b) Atribua 4 à variável b.')
+b = int(4)
+
 print('(c) Atribua à variável c o valor da expressão a * a + b * b')
+c = a * a + b * b
+print(c)

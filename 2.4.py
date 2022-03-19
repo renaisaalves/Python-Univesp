ddc2a686d96b17f9a55f9b3e10605f15e62e22e9
Renaísa <renaisaalves@gmail.com>
2022-03-19 17:46:57 -0300

    Update 2.4.py

--- 2.4.py
+++ 2.4.py
@@ -3,8 +3,14 @@ s1 = 'ant'
 s2 = 'bat'
 s3 = 'cod'
 print('Escreva expressões Python usando s1, s2 e s3 e os operadores + e * a fim de avaliar para:')
-print('')#(a) 'ant bat cod'
-print('')#(b) ant ant ant ant ant ant ant ant ant ant'
-print('')#(c) 'ant bat bat cod cod cod'
-print('')#(d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
-print('')#(e) 'batbatcod batbatcod batbatcod batbatcod batbatcod'
+s4 = s1 + s1 + s3
+s5 = s1 * 6
+s6 = s1 + s2 * 2 + s3 * 3
+s7 = s1, s2, s1, s2, s1, s2 
+s8 = s2 * 2 + s3
+s8 = s8 * 5 
+print(s4)#(a) 'ant bat cod'
+print(s5)#ant ant ant ant ant ant ant ant ant ant'
+print(s6)#(c) 'ant bat bat cod cod cod'
+print(s7)#(d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+print(s8)#(e) 'batbatcod batbatcod batbatcod batbatcod batbatcod'

a7fdd66fe4635d8a545286941682a2319f8874fd
Renaísa <renaisaalves@gmail.com>
2022-03-29 17:49:34 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,12 @@
+import math 
+
+num = input("Digite um número: ") 
+quadrado = math
+
+pow (num, 2) 
+
+cubo = math
+pow(num, 3) 
+raiz = math.sqrt(num) 
+print(f'O numero ao quadrado é {quadrado} e ao cubo é {cubo}') 
+print(f'A raiz quadrada é {raiz:.2f}')

9a6688a9a05aaa131be10fa92c9cb0bc3f62e4db
Renaísa <renaisaalves@gmail.com>
2022-03-29 18:18:24 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,4 @@
-import math 
-
-num = input("Digite um número: ") 
-quadrado = math
-
-pow (num, 2) 
-
-cubo = math
-pow(num, 3) 
-raiz = math.sqrt(num) 
-print(f'O numero ao quadrado é {quadrado} e ao cubo é {cubo}') 
-print(f'A raiz quadrada é {raiz:.2f}')
+base = int(input("Digite a base: ")) 
+altura = int(input("Digite a altura: ")) 
+area = (base * altura) / 2 
+print("A área é", area)  

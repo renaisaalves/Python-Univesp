e335ada105b3ba269209f4e6669be734f4ac6057
Renaísa <renaisaalves@gmail.com>
2022-06-07 10:22:25 -0300

    Update ordenação.py

--- ordenação.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort(* v):
-    for i in range(len(v)-1):
-        for j in range(len(v)-i-1):
-            if(v[j] > v[j+1]):
-                v[j] = v[j+1]
-                v[j+1] = v[j]
-vetor = (25, 57, 48, 37, 12, 92, 86, 33)
-bubble_sort(vetor)

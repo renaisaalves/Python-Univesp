a7811f7f19001aaf8763264701a372878c4b1a9b
Renaísa <renaisaalves@gmail.com>
2022-06-09 15:12:37 -0300

    Update ordenação.py

--- ordenação.py
+++ ordenação.py
@@ -22,5 +22,28 @@ def insertion_sort(v):
 insertion_sort(exemplo)
 
 #MERGE SORT
+
+def intercala(v, ini, meio, fim):
+    L = v[ini:meio+1]
+    R = v[meio+1:fim+1]
+    L.append(999) #sentinela
+    R.append(999) #sentinela
+    i = 0
+    j = 0
+    for k in range(ini, fim+1):
+        if L[i] <= R[j]:
+            v[k] = L[i]
+            i += 1
+        else:
+            v[k] = R[j]
+            j += 1
+            
+def merge_sort(v, ini, fim):
+    if ini < fim:
+        meio = (ini + fim) // 2
+        merge_sort(v, ini, meio)
+        merge_sort(v, meio+1, fim)
+        intercala(v, ini, meio, fim)
+
 #QUICK SORT
 #HEAP SORT

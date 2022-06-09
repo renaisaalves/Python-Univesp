707096ffad678cf9f712e9dba15831ab194f2678
Renaísa <renaisaalves@gmail.com>
2022-06-09 14:21:28 -0300

    Update ordenação.py

--- ordenação.py
+++ ordenação.py
@@ -1,6 +1,13 @@
 #BUBBLE SORT
+def bubble_sort(v):
+    for i in range(len(v)-1): 
+	    for j in range(len(v)-i-1): 
+	        if(v[j] > v[j+1]):
+	            v[j], v[j+1] = v[j+1], v[j]
 
 exemplo = [25, 57, 48, 37, 12, 92, 86, 33]
+bubble_sort(exemplo)
+
 #MERGE SORT
 #QUICK SORT
 #HEAP SORT

aff349d4f2a90dbf9d854683433889d596818c7c
Renaísa <renaisaalves@gmail.com>
2022-06-20 14:06:21 -0300

    Update interface-grid.py

--- interface-grid.py
+++ interface-grid.py
@@ -4,4 +4,5 @@ from tkinter import Tk, Label, RAISED
 root = Tk()
 labels = [['1', '2', '3']
           ['4', '5', '6']
-          ['7', '8', '9']]
+          ['7', '8', '9']
+          ['*', '0', '#']]

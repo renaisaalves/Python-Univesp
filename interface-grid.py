e09092681bf3e07f9c9d0b4f6dc63f1f1ff59430
Renaísa <renaisaalves@gmail.com>
2022-06-20 14:09:15 -0300

    Update interface-grid.py

--- interface-grid.py
+++ interface-grid.py
@@ -6,3 +6,9 @@ labels = [['1', '2', '3']
           ['4', '5', '6']
           ['7', '8', '9']
           ['*', '0', '#']]
+
+for r in range(4): #linhas (row)
+    for c in range(3): #colunas (column)
+        label = Label(root, relief=RAISED, padx=10, text=labels[r][c])
+        label.grid(row=r, column=c)
+root.mainloop()

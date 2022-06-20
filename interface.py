8138ee2554f11265bb69ca7318060fb9a369b135
Renaísa <renaisaalves@gmail.com>
2022-06-20 13:25:03 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,6 +6,11 @@ root = Tk()
 photo = PhotoImage(file='ratinho-flores.gif')
 image = Label(master=root, image=photo)
 image.pack(side=BOTTOM)
-text = Label(master=root, font=("Arial", 18), text='Meu primeiro programa com Interface Gráfica!')
+text = Label(master=root, font=("Calibri", 18), text='Meu primeiro programa com Interface Gráfica!')
 text.pack(side=TOP)
 root.mainloop()
+
+#tkinter é uma biblioteca GUI do Python que permite fazer Interface Gráfica. 
+#Dentro dessa biblioteca, existem várias classes com uma função específica, por exemplo:
+#Tk é a principal janela da uma aplicação, ou seja, ela é a janela que abriga todos os elementos. 
+#Label serve para inserir textos e bitmaps (imagens).

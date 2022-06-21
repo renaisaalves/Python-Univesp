07b7eb02772bf6aa8fc7512d45558f9369eb50d5
Renaísa <renaisaalves@gmail.com>
2022-06-21 07:21:48 -0300

    Create interface-prova.py

---
+++ interface-prova.py
@@ -0,0 +1,17 @@
+#Retirado da prova Univesp
+
+from tkinter import Tk, Button, Label, Entry, END 
+def clicked(): 
+    global entry 
+    name = entry.get() 
+    print('Ola', name) 
+    entry.delete(0, END) 
+ 
+root = Tk() 
+label = Label(root, text='Nome:') 
+label.grid(row=0, column=0) 
+entry = Entry(root) 
+entry.grid(row=0, column=1) 
+button = Button(root, text='OK', command=clicked) 
+button.grid(row=1, column=0, columnspan=2) 
+root.mainloop() 

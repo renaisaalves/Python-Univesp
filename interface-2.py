845ef164cf510c55c6ca3001b98a72b0d9b8f7b4
Renaísa <renaisaalves@gmail.com>
2022-06-21 07:13:14 -0300

    Update interface-2.py

--- interface-2.py
+++ interface-2.py
@@ -4,4 +4,11 @@ from tkinter import Tk, Button, Label
 from tkinter.messagebox import showinfo
 from time import strftime, localtime
 
+def clicked():
+    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S%p\n', localtime())
+showinfo(message=time)
 
+root = Tk()
+button = Button(root, text='Clique', command=clicked)
+button.pack()
+root.mainloop()

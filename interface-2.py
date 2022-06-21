1256b8fbb49ff0f3a5c9f997c36718abf7645b57
Renaísa <renaisaalves@gmail.com>
2022-06-21 07:11:27 -0300

    Update interface-2.py

--- interface-2.py
+++ interface-2.py
@@ -1,1 +1,7 @@
 #Primeiro exemplo: janela com um botão que, quando clicado, exibe o dia e a hora na tela. 
+
+from tkinter import Tk, Button, Label
+from tkinter.messagebox import showinfo
+from time import strftime, localtime
+
+

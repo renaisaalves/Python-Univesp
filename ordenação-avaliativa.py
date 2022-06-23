388071d081cc2675ae367d93f21013334783ccc1
Renaísa <renaisaalves@gmail.com>
2022-06-23 14:29:07 -0300

    Create ordenação-avaliativa.py

---
+++ ordenação-avaliativa.py
@@ -0,0 +1,10 @@
+def ordena(v): 
+    for i in range(1, len(v)): 
+        chave = v[i] 
+        j = i - 1 
+        while j >= 0 and v[j] > chave: 
+            v[j + 1] = v[j] 
+            j -= 1 
+        v[j + 1] = chave
+lista = [25, 57, 48, 37, 12, 92, 86, 33]
+ordena(lista)

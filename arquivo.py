0ed83e370df5e6174c60f5d2038b98144ace0fd3
Renaísa <renaisaalves@gmail.com>
2022-06-29 08:14:53 -0300

    Create arquivo.py

---
+++ arquivo.py
@@ -0,0 +1,5 @@
+#Como manipular um arquivo usando Python. 
+# 1 Abrir um arquivo para leitura ou escrita -> open('example.txt', 'r')
+    # O primeiro argumento é o caminho para o arquivo ('example.text') e o segundo é o modo de abertura ('r')
+# 2. Ler os dados do arquivo ou escrever nele
+# 3. Fechar o arquivo

70b5ecd0b5f2d99cd5ef9f6c31239da85861a135
Renaísa <renaisaalves@gmail.com>
2022-06-29 08:56:19 -0300

    Updates

--- arquivo.py
+++ arquivo.py
@@ -3,3 +3,12 @@
     # O primeiro argumento é o caminho para o arquivo ('example.text') e o segundo é o modo de abertura ('r')
 # 2. Ler os dados do arquivo ou escrever nele
 # 3. Fechar o arquivo
+
+def readFile(filename):
+    infile = open(filename, 'r')
+    content = infile.read()
+    infile.close()
+    wordList = content.split()
+    print(wordList)
+    return len(wordList), len(content)
+n_words, n_chars = readFile('teste.txt')

01ceb6732e54546c0c8210b15b2681561df99c3b
Renaísa <renaisaalves@gmail.com>
2022-06-29 09:08:25 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -5,10 +5,11 @@
 # 3. Fechar o arquivo
 
 def readFile(filename):
-    infile = open(filename, 'r')
-    content = infile.read()
-    infile.close()
-    wordList = content.split() #split é uma função que vai separar cada caractere do arquivo
-    print(wordList)
-    return len(wordList), len(content)
+    infile = open(filename, 'r') #abre o arquivo no modo leitura
+    content = infile.read() #lê os dados do arquivo em modo leitura
+    infile.close() #fecha o arquivo 
+    wordList = content.split() #split é uma função que vai separar cada palavra do arquivo
+    print(wordList) #exibe todas as palavras do texto
+    return len(wordList), len(content) #o retorno será a quantidade de palavras e caracteres
 n_words, n_chars = readFile('testo.txt')
+print(n_words, n_chars) #vai exibir na tela a quantidade de palavras e a quantidade de caracteres.

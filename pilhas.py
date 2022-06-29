e8e4122caa4cedd0165faf2b12df9c85113c8f3d
Renaísa <renaisaalves@gmail.com>
2022-06-29 09:35:07 -0300

    Update pilhas.py

--- pilhas.py
+++ pilhas.py
@@ -2,16 +2,16 @@ class Pilha():
     def __init__(self):
         self.data = []
         
-    def push(self, x):
+    def push(self, x): #empilha um elemento
         self.data.append(x)
         
-    def pop(self):
+    def pop(self): #desempilha um elemento
         if len(self.data) > 0:
             return self.data.pop(-1)
         
-    def top(self):
+    def top(self): #acessa o elemento do topo, sem desempilhá-lo
         if len(self.data) > 0:
             return self.data[-1]
         
-    def empty(self):
+    def empty(self): #verifica se a pilha está vazia
         return len(self.data) > 0

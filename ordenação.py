f7c7dbe6bd6a3c304efd1fe8bebda5f019109d8b
Renaísa <renaisaalves@gmail.com>
2022-07-05 15:42:24 -0300

    Update ordenação.py

--- ordenação.py
+++ ordenação.py
@@ -5,11 +5,10 @@ def bubble_sort(v):
     for i in range(len(v)-1): 
 	    for j in range(len(v)-i-1): 
 	        if(v[j] > v[j+1]):
-                    v[j] = v[j+1]
-                    v[j+1] = v[j]
-
-
-bubble_sort(exemplo)
+                    v[j], v[j+1] = v[j+1], v[j]
+    return v
+bubble = bubble_sort(exemplo)
+print(bubble, 'Bubble Sort')
 #Não funcionou.
 
 def insertion_sort(v):
@@ -20,9 +19,20 @@ def insertion_sort(v):
             v[j+1] = v[j]
             j -= 1
         v[j+1] = x
-insertion_sort(exemplo)
+    return v
+insertion = insertion_sort(exemplo)
+print(insertion, 'Insertion Sort')
 
 #MERGE SORT
+
+           
+'''def merge_sort(v, ini=0, fim=len(v)-1):
+    if ini < fim:
+        meio = (ini + fim) // 2
+        merge_sort(v, ini, meio)
+        merge_sort(v, meio+1, fim)
+        intercala(v, ini, meio, fim)
+    return v
 def intercala(v, ini, meio, fim):
     L = v[ini:meio+1]
     R = v[meio+1:fim+1]
@@ -37,14 +47,7 @@ def intercala(v, ini, meio, fim):
         else:
             v[k] = R[j]
             j += 1
-            
-def merge_sort(v, ini=0, fim=0):
-    if ini < fim:
-        meio = (ini + fim) // 2
-        merge_sort(v, ini, meio)
-        merge_sort(v, meio+1, fim)
-        intercala(v, ini, meio, fim)
-merge_sort(exemplo)
+    return v '''
 
 #QUICK SORT
 def quick_sort(v, ini=0, fim=0):
@@ -65,6 +68,8 @@ def quick_sort(v, ini=0, fim=0):
         quick_sort(v, ini, j)
     if i < fim:
         quick_sort(v, i, fim)
-quick_sort(exemplo)
+    return v
+quick = quick_sort(exemplo)
+print(quick, 'Quick Sort')
 
 #HEAP SORT
